0ecb3fa1fb2697be08949a5f8e6ae199de6c7b5e
kaarturov <kaarturov@sberbank.ru>
2024-02-03 09:00:02 +0700

    repo refine

--- micros/game/src/pages/Game.tsx
+++ micros/game/src/pages/Game.tsx
@@ -1,13 +1,7 @@
 import React, { FC } from 'react';
 
-import Heart from '@src/assets/heart.svg';
-
 const Game: FC = () => {
-  return (
-    <div data-testid='App.Container'>
-      <h1>About us</h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Game;
